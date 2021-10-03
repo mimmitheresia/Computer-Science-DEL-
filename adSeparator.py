853666f26420547ce3486ed0a297ee9fc751d906
Mimmi Andersson <mimmitheresia@gmail.com>
2021-10-03 14:48:48 +0200

    Update adSeparator.py

--- adSeparator.py
+++ adSeparator.py
@@ -1,19 +1,23 @@
 # adSeparator.py reads advertasement dataset from each year (default 2006-2010).
+# Python program to read
+# json file
 import json 
 import re
 
 class FileObjects:
     def __init__(self):
-        self.f_nr = open("nr-by-occupation-06-07.txt","w") #0-07 size: 22.8kB
-        self.f_tx = open("text-by-occupation-06-07.txt","w") #06-07 size: 39.1MB
-        self.f_hd = open("head-by-occupation-06-07.txt","w") #06-07 size: 976kB
-        self.f_tx2 = open("text-by-occupation-06-07-part2.txt","w")
+        self.f_nr = open("nr-by-occupation-10.txt","w",encoding="utf8") #0-07 size: 22.8kB
+        self.f_tx = open("text-by-occupation-10.txt","w",encoding="utf8") #06-07 size: 39.1MB
+        self.f_hd = open("head-by-occupation-10.txt","w",encoding="utf8") #06-07 size: 976kB
     
     def write_to_file(self, occ_mer):
+        
         nr_of_occ = len(occ_mer.text_by_occupation)
-        print(nr_of_occ)
+        print("Number of occupations:" + str(nr_of_occ))
         occ_nr = 0
         for occ in occ_mer.text_by_occupation:
+            if (occ_nr % 100 == 0):
+                print("Writing occ nr: " + str(occ_nr))
             self.f_nr.write(str(occ)+ ":" + str(occ_mer.nr_by_occupation.get(occ)) + "\n")
             self.f_hd.write(str(occ)+ ":" + str(occ_mer.head_by_occupation.get(occ))+ "\n")
             self.f_tx.write(str(occ)+ ":" + str(occ_mer.text_by_occupation.get(occ))+ "\n")    
@@ -49,19 +53,18 @@ class OccupationMerge():
         self.merge_dicts = [self.nr_by_occupation, self.text_by_occupation,self.head_by_occupation]
     
     def add_advertisement(self,label,text,head):
-        new_text = re.sub(' +', ' ',text)
         if label in self.nr_by_occupation:
             #Add nr, text and headline to stored label 
             stored_nr = self.nr_by_occupation.get(label)
             stored_text = self.text_by_occupation.get(label)
             stored_head = self.head_by_occupation.get(label)
             self.nr_by_occupation[label] = stored_nr+1
-            self.text_by_occupation[label] = stored_text + "{" + new_text + "}" 
+            self.text_by_occupation[label] = stored_text + "{" + text + "}" 
             self.head_by_occupation[label] = stored_head + "{" + head + "}"    
         else:
             #Initiating new occupation label
             self.nr_by_occupation[label] = 1
-            self.text_by_occupation[label] = "{" + new_text + "}"
+            self.text_by_occupation[label] = "{" + text + "}"
             self.head_by_occupation[label] = "{" + head + "}"
                
 
@@ -78,12 +81,13 @@ def seperate_by_occupation(occ_mer,adv):
     return valid 
     
   
-def iterate_adv_set(occ_sep,adv_set): 
+def iterate_adv_set(occ_sep,adv_total,adv_set): 
     valid_nr = 0
     adv_nr = 1
     adv_limit = len(adv_set)
-    
     for adv in adv_set:
+        if (adv_nr % 40000 == 0):
+            print("Iterating ad nr: " + str(adv_nr))
         adv_nr +=1
         valid = seperate_by_occupation(occ_sep,adv)
         if valid:
@@ -98,19 +102,17 @@ def main():
     occ_mer = OccupationMerge()
     f_o = FileObjects()
     valid_nr = 0 #Initiating 
-    year_string = ""
-    for year in range(2006,2008):
-        f = open(str(year)+'.json',)
+    for year in range(2010,2011):
+        f = open(str(year)+'.json')
         adv_set = json.load(f)
         adv_total = len(adv_set)
-        #adv_str.print_adv_structure(adv_set[5]) #IS NOT CORRECT 
-        valid_nr = iterate_adv_set(occ_mer, adv_set) 
-        print("Advertisement set from year " + str(year)+ ":")
-        print("Number of adds: " + str(adv_total))
+        #adv_str.print_adv_structure(adv_set[5])
+        print("Advertisement dataset from year " + str(year)+ ":")
+        print("Number of ads: " + str(adv_total))  
+        valid_nr = iterate_adv_set(occ_mer, adv_total, adv_set) 
         print("Valid adds: " + str(valid_nr))
         print("Invalid adds: " + str(adv_total-valid_nr))
         f.close()
-   
     f_o.write_to_file(occ_mer)
     print("End") 
 main()
