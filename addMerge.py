0f9592a048470973c850b0c61210d74b28ffe13b
Mimmi Andersson <mimmitheresia@gmail.com>
2021-10-03 21:13:00 +0200

    Update addMerge.py

--- addMerge.py
+++ addMerge.py
@@ -1,161 +1,115 @@
+import json 
+
+
+class FileObjects:
+    def __init__(self):
+        self.f_nr = open("nr-by-occupation-06.txt","w",encoding="utf8") #0-07 size: 22.8kB
+        self.f_tx = open("text-by-occupation-06.txt","w",encoding="utf8") #06-07 size: 39.1MB
+        self.f_hd = open("head-by-occupation-06.txt","w",encoding="utf8") #06-07 size: 976kB
+    
+    def write_to_file(self, occ_mer):
+        
+        nr_of_occ = len(occ_mer.text_by_occupation)
+        print("Number of occupations:" + str(nr_of_occ))
+        occ_nr = 0
+        for occ in occ_mer.text_by_occupation:
+            if (occ_nr % 100 == 0):
+                print("Writing occ nr: " + str(occ_nr))
+            self.f_nr.write(str(occ)+ ":" + str(occ_mer.nr_by_occupation.get(occ)) + "\n")
+            self.f_hd.write(str(occ)+ ":" + str(occ_mer.head_by_occupation.get(occ))+ "\n")
+            self.f_tx.write(str(occ)+ ":" + str(occ_mer.text_by_occupation.get(occ))+ "\n")    
+            occ_nr +=1 
+        print("Number of total occupations: " + str(occ_nr))     
+          
+class AdvStructure():
+    def return_next_level(self,attr_level, level=0):
+        level += 1
+        for attribute in attr_level:
+            if type(attr_level.get(attribute)) == dict:
+                print(str("  "*level) + 'L' + str(level) + " " + str(attribute))   
+                self.return_next_level(attr_level.get(attribute), level)
+            else: 
+                print(str("  "*level)+ 'L' + str(level) + " " + str(attribute) + ": " + str(attr_level.get(attribute)))
+        return
+
+    def print_adv_structure(self,adv):
+        for attribute in adv: 
+            level = 0
+            if type(adv.get(attribute)) == dict: 
+                print('L' + str(level) + " " + str(attribute))
+                self.return_next_level(adv.get(attribute),0)
+            else:
+                print('L' + str(level) + " " + str(attribute) + ": " + str(adv.get(attribute)))
+
+
 class OccupationMerge():
     def __init__(self):
         self.nr_by_occupation = {}
         self.text_by_occupation = {}
         self.head_by_occupation = {}
-
-    def updateNumber(self,occ,nr):
-        stored_nr = self.nr_by_occupation.get(occ)
-        total_nr = stored_nr + nr 
-        self.nr_by_occupation[occ] = total_nr
+        self.merge_dicts = [self.nr_by_occupation, self.text_by_occupation,self.head_by_occupation]
     
-    def updateHead(self,occ,head):
-        stored_head = self.head_by_occupation.get(occ)
-        total_head = stored_head + head 
-        self.head_by_occupation[occ] = total_head
-
-    def updateText(self,occ,text):
-        stored_text = self.text_by_occupation.get(occ)
-        total_text = stored_text + text
-        self.text_by_occupation[occ] = total_text
-        
-    def addOccNumber(self,row,row_nr):
-        if (row_nr % 500 == 0):
-            print("Adding number from row: " + str(row_nr))
-        index = row.rfind(":")
-        occ = row[:index].strip()
-        occ_nr = int(row[index+1:])
-        #if row.startswith('Projektledare'):
-            #print("Occupation in number:" + str(occ)+ " " + str(len(occ)))
-        if occ in self.nr_by_occupation:
-            self.updateNumber(occ,occ_nr)
+    def add_advertisement(self,label,text,head):
+        if label in self.nr_by_occupation:
+            #Add nr, text and headline to stored label 
+            stored_nr = self.nr_by_occupation.get(label)
+            stored_text = self.text_by_occupation.get(label)
+            stored_head = self.head_by_occupation.get(label)
+            self.nr_by_occupation[label] = stored_nr+1
+            self.text_by_occupation[label] = stored_text + "{" + text + "}" 
+            self.head_by_occupation[label] = stored_head + "{" + head + "}"    
         else:
-            self.nr_by_occupation[occ] = occ_nr
+            #Initiating new occupation label
+            self.nr_by_occupation[label] = 1
+            self.text_by_occupation[label] = "{" + text + "}"
+            self.head_by_occupation[label] = "{" + head + "}"
+               
 
-    def addOccHeads(self,row,row_nr):
-        if (row_nr % 500 == 0):
-            print("Adding headlines from row: " + str(row_nr))
-        index = row.index("{")
-        occ = row[:(index-1)].rstrip()
-        occ_heads = row[index:].strip("\n")
-        #if row.startswith('Projektledare'):
-            #print("Occupation in heads:" + str(occ) + " " + str(len(occ)))
-        if occ in self.head_by_occupation:
-            self.updateHead(occ,occ_heads)
-        else:
-            self.head_by_occupation[occ] = occ_heads
+def seperate_by_occupation(occ_mer,adv):
+    valid = True 
+    occ_label = adv["occupation"]["label"]
+    des_text = adv["description"]["text"]
+    headline = adv["headline"]
+    if occ_label == None or (des_text == None) or (headline == None):
+        valid = False 
+    else:
 
-    def addOccText(self,row, row_nr):
-        if (row_nr % 500 == 0):
-            print("Adding text from row: " + str(row_nr)) 
-        row = row.split(":")
-        if len(row) == 2:
-            occ = row[0]
-            occ_text = row[1].strip("\n")
-        if len(row) > 2:
-            occ_text = row[len(row)-1]
-            occ = ""
-            for w in row:
-                occ += w
-        if occ in self.text_by_occupation:
-            self.updateText(occ,occ_text)
-        else:
-            self.text_by_occupation[occ] = occ_text
+        occ_mer.add_advertisement(occ_label, des_text, headline)
+    return valid 
     
-    def sort_desc(self):
-        pass
-        #sorted = sorted(occ_dic.items(),key=operator.itemgetter(1),reverse=True)
+  
+def iterate_adv_set(occ_sep,adv_total,adv_set): 
+    valid_nr = 0
+    adv_nr = 1
+    adv_limit = len(adv_set)
+    for adv in adv_set:
+        if (adv_nr % 40000 == 0):
+            print("Iterating ad nr: " + str(adv_nr))
+        adv_nr +=1
+        valid = seperate_by_occupation(occ_sep,adv)
+        if valid:
+            valid_nr +=1 
+        if adv_nr > adv_limit:
+            break
+    return valid_nr
 
 
-
-def merge_file_by_year(occ_mer,path, path_type):
-    #Add from 2006-2007
-    f = open(str(path)+"-06-07.txt", "r")
-    print("Reading from " + str(path)+"-06-07.txt...")
-    row_nr = 0 
-    for row in f:
-        if path_type == "nr":
-            occ_mer.addOccNumber(row,row_nr)
-        if path_type == "head":
-            occ_mer.addOccHeads(row,row_nr)
-        if path_type == "text":
-            occ_mer.addOccText(row,row_nr)
-        row_nr += 1
-    f.close()
-
-    #Merge from 2008
-    f = open(str(path)+"-08.txt", "r")
-    print("Reading from " + str(path)+"-08.txt..")
-    row_nr = 0 
-    for row in f:
-        if path_type == "nr":
-            occ_mer.addOccNumber(row,row_nr)
-        if path_type == "head":
-            occ_mer.addOccHeads(row,row_nr)
-        if path_type == "text":
-            occ_mer.addOccText(row,row_nr)
-        row_nr += 1
-    f.close()
-    
-    #Merge from 2009
-    f = open(str(path)+"-09.txt", "r")
-    print("Reading from " + str(path)+"-09.txt..")
-    row_nr = 0 
-    for row in f:
-        if path_type == "nr":
-            occ_mer.addOccNumber(row,row_nr)
-        if path_type == "head":
-            occ_mer.addOccHeads(row,row_nr)
-        if path_type == "text":
-            occ_mer.addOccText(row,row_nr)
-        row_nr += 1
-    f.close()
-
-    #Merge from 2010
-    f = open(str(path)+"-10.txt", "r")
-    print("Reading from " + str(path)+"-10.txt..")
-    row_nr = 0 
-    for row in f:
-        if path_type == "nr":
-            occ_mer.addOccNumber(row,row_nr)
-        if path_type == "head":
-            occ_mer.addOccHeads(row,row_nr)
-        if path_type == "text":
-            occ_mer.addOccText(row,row_nr)
-        row_nr += 1
-    f.close()
-    
-def control_check(occ_mer):
-    invalid = 0
-    for occ in occ_mer.head_by_occupation:
-        if occ not in occ_mer.nr_by_occupation:
-            print(occ + " " + str(len(occ)))
-            invalid +=1 
-
-    print("Invalid: " + str(invalid))  
-    
 def main():
+    adv_str = AdvStructure() 
     occ_mer = OccupationMerge()
-    path_list = ["nr-by-occupation", "head-by-occupation", "text-by-occupation" ]
-    types = ["nr", "head", "text"]
-    i = 0 
-    for i in range(0,2):
-        merge_file_by_year(occ_mer, path_list[i], types[i])
-        if  types[i] == "nr":
-            for occ in occ_mer.nr_by_occupation:
-                
-                print(str(occ)+ ": " + str(occ_mer.nr_by_occupation.get(occ)))
-            print("Number of occupations: " + str(len(occ_mer.nr_by_occupation)))
-    
-        if  types[i] == "head":
-            for occ in occ_mer.head_by_occupation:
-                pass
-                #print(str(occ)+ ": " + str(occ_mer.head_by_occupation.get(occ)))
-            print("Number of occupations: " + str(len(occ_mer.head_by_occupation)))
-
-        if  types[i] == "text":
-            for occ in occ_mer.text_by_occupation:
-                print(str(occ)+ ": " + str(occ_mer.text_by_occupation.get(occ)))
-            print("Number of occupations: " + str(len(occ_mer.text_by_occupation)))  
-        #control_check(occ_mer)
+    f_o = FileObjects()
+    valid_nr = 0 #Initiating 
+    for year in range(2010,2011):
+        f = open(str(year)+'.json')
+        adv_set = json.load(f)
+        adv_total = len(adv_set)
+        #adv_str.print_adv_structure(adv_set[5])
+        print("Advertisement dataset from year " + str(year)+ ":")
+        print("Number of ads: " + str(adv_total))  
+        valid_nr = iterate_adv_set(occ_mer, adv_total, adv_set) 
+        print("Valid adds: " + str(valid_nr))
+        print("Invalid adds: " + str(adv_total-valid_nr))
+        f.close()
+    #f_o.write_to_file(occ_mer)
+    print("End") 
 main()
