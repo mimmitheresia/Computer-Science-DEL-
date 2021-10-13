8280367d130054d645e7bd834b66727d0220da9e
ewelynstrandberg <81417687+ewelynstrandberg@users.noreply.github.com>
2021-10-13 09:16:31 +0200

    Update occupationGenderClassifier.py
    Update from Ewelyn's computer

--- occupationGenderClassifier.py
+++ occupationGenderClassifier.py
@@ -16,7 +16,7 @@ class OccupationAds:
 def readAdTerms():
     ads_obj_dict = {}
     ads_occ_list = []
-    f = open("2006-2010-occupation-frequency.txt","r",encoding="utf8")
+    f = open("2006-2010-occupation-frequency.txt","r",encoding='utf8')
     for row in f:
         index = row.rfind(":")
         occ = row[:index].strip()
@@ -56,10 +56,11 @@ def return_manual_match(stat_gender, ad_occ,ad_obj, gender):
     if gender == "Women":
        # _old_accurate_gender_ads = {0:0, 2:0, 3:"ALL", 4:0, 5:0, 8:0, 9:0, 10:[0,1],11:0, 14:3} 
         accurate_gender_ads = {0:[0,1,2,3,4,5,7,8], 1: "ALL" , 2:"ALL", 3: "ALL", 4:[0,1,3], 5:[0,5], 6:2, 7:[3,5], 8:0, 9:0, 10:[0,1,5],11:0,12:[0,],13:0, 14:3}
-        f = open("women-ad-occupations.txt", "w", encoding = "utf-8")
+        f = open("women-ad-occupations.txt", "w", encoding = 'utf-8')
     if gender == "Men":
-        accurate_gender_ads = { 0:0, 11:6, 18:[0,1,4], 19:0, 31:0,32:0, 36:[0,3], 37:[1,2], 38:[1,2], 39:0, 40:[0,1], 55:0}
-        f = open("men-ad-occupations.txt", "w", encoding = "utf-8")
+        #accurate_gender_ads = { 0:0, 11:6, 18:[0,1,4], 19:0, 31:0,32:0, 36:[0,3], 37:[1,2], 38:[1,2], 39:0, 40:[0,1], 55:0}
+        accurate_gender_ads = {0:[0,1,2,3,5,9], 1:[0,2,3], 3:[0,2,4,5,6], 4:[0,1,5,8], 5:[0,2,3], 6:0, 7:[0,2,6,9,10], 8:[0,2,3,4,5,6,8,9], 9:[0,2], 10:[0,1,2,3,4,5,9,10], 11:[0,2,3,7,9,10], 12:[0,1,6,10], 13:[0,1,2,3,4,5,6,9], 14:[1,2,4,6], 15:[0,4,5,6], 16:[0,1], 17:7, 18:[0,1,3,4,6,7], 19:[0,1,2,3,4,5,7,9], 20:0, 22:[0,9], 23:[0,3,6,10], 24:[0,1,2], 25:[0,3,4,6],26:[0,1],27:[0,1,2,3,4,6,7], 28:[0,1,3,4,6,7], 30:0, 31:[0,8],32:[0,1,3,4,7], 33:[0,1,2,7], 34:[0,1,2,3], 35:[0,1,4,5], 36:[2,8,9], 37:[0,1,6,7,8], 38:[1,2,3,5],39:[0,8,9], 40:[0,1,2,3,6,8,9], 41:[0,1,3,4,5,8], 43:[3,4], 44:[0,1,2,4,5,7,9], 45:[4,7], 46:[0,1,2,3,6], 47:"ALL", 48:[0,1,2,3,4,5,6,7,8,9], 49:[2,3,5,7,8,10], 51:[0,1,4,9,10], 52:[0,2,3,4,6,7,8,9,10], 53:"ALL", 54:[0,1,2,3,4], 55:[0,1], 56:[1,2,4,5,8,10], 57:[3,5,9], 58:[0,3], 59:"ALL", 60:[1,2,9], 62:[1,2,3,4,5,6,8,9], 63:[0,1,4,5,6,7,10], 64:[0,1,2,3,5,6,7,8,9,10], 66:[0,1,4,6,7,9,10], 67:[0,1,3,4,9], 68:[0,1,2], 70:[0,1,4], 71:[1,7], 72:0}
+        f = open("men-ad-occupations.txt", "w", encoding = 'utf-8')
     
     i  = 1
     total_nr = 0 
@@ -128,14 +129,17 @@ def returnDominatedOccupations(statistic_obj_dict,gender):
         if gender == "Men":
             if obj.ratio_men > 85:
                 stat_gender.append(obj)
+        if gender == "Neutral":
+            if (obj.ratio_men) < 60 and obj.ratio_men > 40:
+                stat_gender.append(obj)
     return stat_gender
 
 def readStatisticTerms():
     statistic_occ_list = []
     statistic_obj_dict = {}
-    f = open("statistics07-all-occupations.txt", "r")
+    f = open("statistics07-all-occupations.txt", "r", encoding="utf-8")
     nr = 0 
-    for row in f: #occupation  #total  #women-%  #women-mean-age  #men-%  #men-mean-age
+    for row in f: 
         if nr > 2:
             i = 0
             for chr in row:
@@ -155,7 +159,7 @@ def readStatisticTerms():
     return statistic_obj_dict
 
 def main():
-    gender = "Men"
+    gender = "Neutral"
     statistic_obj_dict = readStatisticTerms()
     ads_obj_dict, ads_occ_list = readAdTerms()
     stat_gender = returnDominatedOccupations(statistic_obj_dict, gender)
