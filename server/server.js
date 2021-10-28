2fbc74c65c13ece29185c52c26bd0826dd7c4c97
Mimmi Andersson <mimmitheresia@gmail.com>
2021-10-28 23:50:30 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,15 +4,15 @@ class LoggedInUsers {
   constructor() {
     this.loggedIn = {};
     this.targetButton = {};
+    this.PreviousDescription = {};
   }
   addUser(username) {
     this.loggedIn[username] = true;
   }
   setTarget(username,gender) {
-    console.log("in set target:");
+    console.log("Set target: " + username + ":" + gender );
     console.log(gender)
     this.targetButton[username] = gender;
-    console.log(JSON.stringify(this.targetButton));
   }
   getTarget(username) {
     console.log("in getTarget:");
@@ -22,6 +22,13 @@ class LoggedInUsers {
       return this.targetButton[username]
     } else {
       return null;}
+  }
+
+  setPreviousDescription(username, classified_des) {
+    this.PreviousDescription[username] =classified_des;
+  }
+  getPreviousDescription(username){
+    return this.PreviousDescription[username];
   }
 }
 
@@ -120,8 +127,6 @@ app.post('/authenticate', (req, res) => {
           }
           if ((req.body.username.length) < 5) {
             res.redirect('/?registration= not successful. UserID needs to be at least 5 characters');
-          } else if (!(usernameContainsDigit) || !(usernameContainsLetter)) {
-            res.redirect('/?registration= not successful. UserID needs to contain at least one letter and one digit');
           } else if ((req.body.password.length) < 5) {
             res.redirect('/?registration= not successful. Password needs to be at least 5 characters');
           } else if (!(passwordContainsDigit) || !(passwordContainsLetter)) {
@@ -157,10 +162,22 @@ app.post('/analyzeaction*', (req, res) => {
   const username = (req.url).split(":")[1];
   console.log(username);
   const target = users.getTarget(username);
+  var ongoingGame =  (req.url).split(":")[3];
+  console.log("Ongoing game");
+  console.log(ongoingGame);
   const des = req.body.hidden;
   if (target){
     if (des) {
-      const result = "{[Jag] [n] [0.5] []} {[söker] [n] [0.5] []} {[en] [n] [0.5] []} {[redig,] [m] [0.8] [ambitiös,framåt]} {[stark] [m] [0.9] [uthållig]} {[och] [n] [0.5] []} {[vacker] [w] [0.9] []} {[kollega.] [n] [0.5] []}";
+      console.log("Description:");
+      console.log(des);
+      const result = "{[hej] [W] [0.6666666666666666] []} {[på] [W] [0.8137254901960784] []} {[dig] [M] [0.6113744075829385] []} {[vi] [N] [0.5] []} {[söker] [N] [0.5] []} {[en] [M] [0.7536764705882353] []} {[redig,] [M] [1.0] [förträfflig;1.0,ordentlig;0.8344370860927153]} {[stark] [M] [0.6294416243654822] [bärkraftig;1.0,markant;1.0]} {[och] [ ] [0.5] []} {[trevlig] [W] [0.6230339928970066] []} {[kollega] [W] [0.6892551892551892] []} {[genast!] [M] [0.8333333333333333] [omedelbart;1.0,på;0.8137254901960784,med;0.7500000000000001]}"
+      if (!ongoingGame) {
+        users.setPreviousDescription(result);
+      }
+      if (ongoingGame){
+        
+      }
+      //const result = "{[Jag] [n] [0.5] []} {[söker] [n] [0.5] []} {[en] [n] [0.5] []} {[redig,] [m] [0.8] [ambitiös,framåt]} {[stark] [m] [0.9] [uthållig]} {[och] [n] [0.5] []} {[vacker] [w] [0.9] []} {[kollega.] [n] [0.5] []}";
       res.redirect(`/userprofile/?user=${username}&target=${target}&analyze= Analyzing...&description=${des}&result=${result}`)
     }
     else {
